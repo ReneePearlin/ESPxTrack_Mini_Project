d63fb8c0938013d0b8c0ef8352a9c3c0e699ccd0
Renee Pearlin <reneepearlin@gmail.com>
2025-04-12 17:01:59 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,12 @@
 // Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "yourapp.firebaseapp.com",
-  projectId: "yourapp",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyATTUPboq4w0nwxubk5e5tL9ujOiSx03aU",
+  authDomain: "trackmyride-5524a.firebaseapp.com",
+  projectId: "trackmyride-5524a",
+  storageBucket: "trackmyride-5524a.firebasestorage.app",
+  messagingSenderId: "275723082314",
+  appId: "1:275723082314:web:54b9c80fecba4e414b24b3",
+  measurementId: "G-9P4N8QVMTE"
 };
 
 firebase.initializeApp(firebaseConfig);
